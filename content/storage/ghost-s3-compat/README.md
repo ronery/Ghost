b3469a10396957fdcad1ee9d19d8e984b2bef089
Ronery <ronery.souza@gmail.com>
2020-08-16 09:51:03 -0300

    Ghost s3

---
+++ content/storage/ghost-s3-compat/README.md
@@ -0,0 +1,69 @@
+# Ghost S3 Storage, compatability mode
+
+This module allows you to read and write images from Amazon S3 instead of
+storing them locally.
+
+After installing, new images that you save will use an absolute URL to S3. Any
+requests to `/content/images/` will be proxied to S3, so that any previous
+images in your blog will not be affected.
+
+## Installation
+
+You will need to have a the custom storage module directly in your project
+directory, the easiest way to do this is:
+
+```bash
+$ npm install ghost-s3-compat
+$ mkdir content/storage
+$ cp -r node_modules/ghost-s3-compat content/storage/ghost-s3-compat
+```
+
+## Configuration
+
+Create new IAM User with permissions to get object from that bucket. Save the
+`ACCESS_KEY` and `ACCESS_SECRET_KEY`.
+
+In `config.js`, add a `storage` block for each environment.
+
+```javascript
+    storage: {
+        active: 'ghost-s3',
+        'ghost-s3': {
+            accessKeyId: 'Put_your_access_key_here',
+            secretAccessKey: 'Put_your_secret_key_here',
+            bucket: 'Put_your_bucket_name_here',
+            region: 'Put_your_bucket_region_here'
+        }
+    },
+```
+
+
+### Asset host
+
+You can add `assetHost` to your config to specify a virtual host url. This is
+most frequently used with a content delivery network (CDN) such as CloudFront,
+CloudFlare, or others. The modified `storage` block would be:
+
+```javascript
+    storage: {
+        active: 'ghost-s3',
+        'ghost-s3': {
+            accessKeyId: 'ACCESS_KEY',
+            secretAccessKey: 'SECRET_ACCESS_KEY',
+            bucket: 'S3_BUCKET_NAME',
+            region: 'S3_REGION',
+            assetHost: 'https://cdn.yourdomain.com/'
+        }
+    }
+```
+
+You can add `assetHost` to your config to specify a virtual host url. For more
+information, [read this section](http://docs.aws.amazon.com/AmazonS3/latest/dev/VirtualHosting.html)
+in the AWS docs.
+
+## Copyright & License
+
+- Original work Copyright (c) 2015 Hoang Pham Huu <phamhuuhoang@gmail.com>
+- Modified work Copyright (c) 2016 Curiosity Media, Inc.
+
+Released under the [MIT license](https://github.com/spanishdict/ghost-s3-compat/blob/master/LICENSE).
---
+++ content/storage/ghost-s3-compat/index.js
@@ -0,0 +1,137 @@
+'use strict';
+
+// # S3 storage module for Ghost blog http://ghost.org/
+var fs = require('fs');
+var path = require('path');
+var Bluebird = require('bluebird');
+var AWS = require('aws-sdk-promise');
+var readFileAsync = Bluebird.promisify(fs.readFile);
+var options = {};
+var util = require('util');
+var BaseStore;
+try {
+    BaseStore = require('ghost/core/server/storage/base');
+} catch (e) {
+    if (e.code !== 'MODULE_NOT_FOUND') throw e;
+    BaseStore = require(path.join(process.cwd(), 'core/server/storage/base'));
+}
+
+function S3Store(config) {
+    BaseStore.call(this);
+    options = config;
+}
+
+util.inherits(S3Store, BaseStore);
+
+ /**
+ * Return the URL where image assets can be read.
+ * @param  {String} bucket [AWS S3 bucket name]
+ * @return {String}        [path-style URL of the S3 bucket]
+ */
+function getAwsPath(bucket) {
+    var awsRegion = (options.region == 'us-east-1') ? 's3' : 's3-' + options.region;
+    var awsPath = options.assetHost ? options.assetHost : 'https://' + awsRegion + '.amazonaws.com/' + options.bucket + '/';
+    return awsPath;
+}
+
+function logError(error) {
+    console.log('error in ghost-s3', error);
+}
+
+function logInfo(info) {
+    console.log('info in ghost-s3', info);
+}
+
+function getTargetName(image, targetDir) {
+    var ext = path.extname(image.name);
+    var name = path.basename(image.name, ext).replace(/\W/g, '_');
+
+    return targetDir + name + '-' + Date.now() + ext;
+}
+
+function validOptions(opts) {
+    return (opts.accessKeyId &&
+        opts.secretAccessKey &&
+        opts.bucket &&
+        opts.region);
+}
+
+S3Store.prototype.save = function save(image) {
+    if (!validOptions(options)) {
+      return Bluebird.reject('ghost-s3 is not configured');
+    }
+
+    var targetDir = this.getTargetDir();
+    var targetFilename = getTargetName(image, targetDir);
+
+    var s3 = new AWS.S3({
+        accessKeyId: options.accessKeyId,
+        secretAccessKey: options.secretAccessKey,
+        bucket: options.bucket,
+        region: options.region
+    });
+
+    return readFileAsync(image.path)
+        .then(function(buffer) {
+            var params = {
+                ACL: 'public-read',
+                Bucket: options.bucket,
+                Key: targetFilename,
+                Body: buffer,
+                ContentType: image.type,
+                CacheControl: 'max-age=' + (1000 * 365 * 24 * 60 * 60) // 365 days
+            };
+
+            return s3.putObject(params).promise();
+        })
+        .tap(function() {
+            logInfo('ghost-s3', 'Temp uploaded file path: ' + image.path);
+        })
+        .then(function(results) {
+            var awsPath = getAwsPath(options.bucket);
+            return Bluebird.resolve(awsPath + targetFilename);
+        })
+        .catch(function(err) {
+            logError(err);
+            throw err;
+        });
+};
+
+// middleware for serving the files
+S3Store.prototype.serve = function serve() {
+    var s3 = new AWS.S3({
+        accessKeyId: options.accessKeyId,
+        secretAccessKey: options.secretAccessKey,
+        bucket: options.bucket,
+        region: options.region
+    });
+
+    return function (req, res, next) {
+        var params = {
+            Bucket: options.bucket,
+            Key: req.path.replace(/^\//, '')
+        };
+
+        s3.getObject(params)
+            .on('httpHeaders', function(statusCode, headers, response) {
+                res.set(headers);
+            })
+            .createReadStream()
+            .on('error', function(err) {
+                logError(err);
+                res.status(404);
+                next();
+            })
+            .pipe(res);
+    };
+};
+
+// noop because Ghost requires this function to be defined
+// TODO: implement
+S3Store.prototype.exists = function exists() {};
+
+// noop because Ghost requires this function to be defined
+// TODO: implement
+S3Store.prototype.delete = function deleteFile() {};
+
+module.exports = S3Store;
